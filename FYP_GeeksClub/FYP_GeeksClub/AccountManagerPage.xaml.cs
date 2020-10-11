da48dcf04860abe146521899c3671ed73c50a6a1
archer3783 <55689974+archer3783@users.noreply.github.com>
2020-10-11 11:42:14 +0800

    ffffddfdsf

---
+++ FYP_GeeksClub/FYP_GeeksClub/AccountManagerPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FYP_GeeksClub
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AccountManagerPage : ContentPage
+    {
+        
+        public AccountManagerPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            var tabbedPage = this.Parent as TabbedPage;
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                    if (Device.OS == TargetPlatform.Android)
+                    {
+                    tabbedPage.CurrentPage = new HomePage();
+                    }
+                
+            });
+            return true;
+        }
+    }
+
+
+}
--- FYP_GeeksClub/FYP_GeeksClub/HomeTabbed.xaml.cs
+++ FYP_GeeksClub/FYP_GeeksClub/HomeTabbed.xaml.cs
@@ -12,9 +12,23 @@ namespace FYP_GeeksClub
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomeTabbed : TabbedPage
     {
+        readonly Page tab1Page;
+        readonly Page tab2Page;
+
         public HomeTabbed()
-        {
+        {   
             InitializeComponent();
+
+            tab1Page = new HomePage() { Title = "Home", IconImageSource = "Home.png"};
+            tab2Page = new AccountManagerPage() { Title = "User" , IconImageSource = "User.png"};
+
+            Children.Add(tab1Page);
+            Children.Add(tab2Page);
+
+            
+            
         }
+
+        
     }
 }
