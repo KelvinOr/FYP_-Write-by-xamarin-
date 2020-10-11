021f91b87542ef33240b3cabe0730c57470539d5
archer3783 <55689974+archer3783@users.noreply.github.com>
2020-10-11 15:11:47 +0800

    finish user detail

--- FYP_GeeksClub/FYP_GeeksClub/AccountManagerPage.xaml.cs
+++ FYP_GeeksClub/FYP_GeeksClub/AccountManagerPage.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using FYP_GeeksClub.Form;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +16,14 @@ namespace FYP_GeeksClub
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AccountManagerPage : ContentPage
     {
-        
+
+        FirebaseClient firebaseClient = new FirebaseClient("https://hareware-59ccb.firebaseio.com/");
+
         public AccountManagerPage()
         {
             InitializeComponent();
+
+            GetUserAccountDetails();
         }
 
         protected override bool OnBackButtonPressed()
@@ -28,10 +36,56 @@ namespace FYP_GeeksClub
                     {
                     tabbedPage.CurrentPage = new HomePage();
                     }
-                
             });
             return true;
         }
+
+        async private void Save_Clicked(object sender, EventArgs e)
+        {
+
+            var Check = (await firebaseClient.Child("UserAccountDetail").OnceAsync<UserAccountDetail>()).Where(
+                a => a.Object.Email == Preferences.Get("email", "").ToString()).FirstOrDefault();
+
+            if (Check == null)
+            {
+                await firebaseClient.Child("UserAccountDetail").PostAsync(new UserAccountDetail()
+                {
+                    Email = Preferences.Get("email", "").ToString(),
+                    UserName = UserName.Text.ToString()
+                });
+            } else
+            {
+                var Update = (await firebaseClient.Child("UserAccountDetail").OnceAsync<UserAccountDetail>()).Where(
+                    a => a.Object.Email == Preferences.Get("email", "").ToString()).FirstOrDefault();
+                await firebaseClient.Child("UserAccountDetail").Child(Update.Key).PutAsync(new UserAccountDetail()
+                {
+                    Email = Preferences.Get("email", "").ToString(),
+                    UserName = UserName.Text.ToString()
+                });
+                    
+            }
+
+            GetUserAccountDetails();
+
+        }
+
+
+        public async void GetUserAccountDetails()
+        {
+            var GetAccount = (await firebaseClient
+                  .Child("UserAccountDetail")
+                  .OnceAsync<UserAccountDetail>()).Where(a => a.Object.Email == Preferences.Get("email", "").ToString()).FirstOrDefault();
+
+            if (GetAccount != null)
+            {
+                lb_user.Text = GetAccount.Object.UserName.ToString();
+            }
+            else
+            {
+                lb_user.Text = "null";
+            }
+        }
+
     }
 
 
---
+++ FYP_GeeksClub/FYP_GeeksClub/Form/UserAccountDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FYP_GeeksClub.Form
+{
+    public class UserAccountDetail
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
