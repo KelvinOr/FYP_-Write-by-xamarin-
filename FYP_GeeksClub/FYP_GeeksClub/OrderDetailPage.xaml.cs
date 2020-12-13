c37f65eb848d2dbbb6ae4c2ce5aca73445265925
humanity <orkevin947@gmail.com>
2020-12-13 12:27:30 +0800

    newPage

---
+++ FYP_GeeksClub/FYP_GeeksClub/OrderDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FYP_GeeksClub
+{
+    public partial class OrderDetailPage : ContentPage
+    {
+        public OrderDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
