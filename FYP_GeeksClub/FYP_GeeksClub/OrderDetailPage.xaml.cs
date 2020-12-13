914ff639eb655d8f34100f73714116e0039fed13
humanity <orkevin947@gmail.com>
2020-12-13 15:43:48 +0800

    UI building

--- FYP_GeeksClub/FYP_GeeksClub/OrderDetailPage.xaml.cs
+++ FYP_GeeksClub/FYP_GeeksClub/OrderDetailPage.xaml.cs
@@ -8,9 +8,22 @@ namespace FYP_GeeksClub
 {
     public partial class OrderDetailPage : ContentPage
     {
+        public string imageURL;
+        public string title;
+        public string price;
+
         public OrderDetailPage(ShopItemDetail shopItemDetail)
         {
             InitializeComponent();
+
+            imageURL = shopItemDetail.imageURL;
+            title = shopItemDetail.title;
+            price = shopItemDetail.price.ToString();
+
+            img_sH.IsVisible = shopItemDetail.saleIng;
+            img_Image.Source = shopItemDetail.imageURL;
+            lb_title.Text = title;
+            lb_price.Text = price;
             
         }
     }
